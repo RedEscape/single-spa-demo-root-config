c4c0b5c646556f52cb6c615269f39fcf582329ce
Danil.Krylov <danil.krylov@onlineinterviewer.ru>
2022-02-10 14:54:10 +0300

    add server

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 express()
-    .use(express.static(path.join(__dirname, "dist")))
-    .get("*", (req, res) => {
-        res.sendFile("index.html", { root: "dist" });
-    })
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .use(express.static(path.join(__dirname, "dist")))
+  .get("*", (req, res) => {
+    res.sendFile("index.html", { root: "dist" });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
